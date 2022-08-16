00a46dc9973b3bf8cd30ce8bb4de5d5b64f0c4b4
ConnieSB1 <108825968+ConnieSB1@users.noreply.github.com>
2022-08-16 18:12:59 +0100

    connie 1608
    - created buttons for each material in html + css
    
    - started coding the buttons in javascript to hide or reveal table rows- at the moment, trs are linked and will disappear on button click, but won't reappear. I think it's because I'm using an if/else statement and need to use a loop but can't figure it out! Help appreciated

---
+++ compare style.js
@@ -0,0 +1,28 @@
+function toggle(Bamboo) {
+    if (Bamboo.style.visibility = 'visible'){
+    Bamboo.style.visibility = 'hidden';
+    }
+    else {alert("error")}
+}
+
+function toggle(Hemp) {
+    if (Hemp.style.visibility = 'visible'){
+    Hemp.style.visibility = 'hidden';
+    }
+    else {alert("error")}
+}
+
+function toggle(Cork) {
+    if (Cork.style.visibility = 'visible'){
+    Cork.style.visibility = 'hidden';
+    }
+    else {alert("error")}
+}
+
+function toggle(Cotton) {
+    if (Cotton.style.visibility = 'visible'){
+    Cotton.style.visibility = 'hidden';
+    }
+    else {alert("error")}
+}
+
